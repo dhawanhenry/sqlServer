d5a2770fca33f69e475c7476671ad68315ebf43e
dhawanhenry <dhawan.henry@gmail.com>
2020-01-26 01:05:46 +0100

    chapter 3

--- 3 Retrieve Data from Multiple Tables/SQLQuery7.sql
+++ 3 Retrieve Data from Multiple Tables/SQLQuery7.sql
@@ -1,10 +1,12 @@
 SELECT * FROM emp;
 
-CREATE TABLE emp (
+DROP TABLE emp;
+
+CREATE TABLE emp(
 	EmployeeID NUMERIC(4) NOT NULL,
 	FirstName VARCHAR(40) NOT NULL,
 	LastName VARCHAR(40) NOT NULL,
-	Address VARCHAR(60) NOT NULL
+	Address VARCHAR(60)
 );
 
 ALTER TABLE emp
@@ -13,53 +15,52 @@ ALTER COLUMN Address VARCHAR(60);
 ALTER TABLE emp
 	ADD Address VARCHAR (50) NOT NULL;
 
- INSERT INTO employee (
-	EmployeeID,
-	FirstName,
-	LastName,
-	Address
-)
+ INSERT INTO emp(
+			EmployeeID,
+			FirstName,
+			LastName,
+			Address
+			)
 
-VALUES (
-	(
-		101,
-		'Jackson',
-		'Joe',
-		'Mumbai'
-	),
+	VALUES (	
+			101,
+			'Jackson',
+			'Joe',
+			'Mumbai'
+			),
 
-	(
-		102,
-		'Smith',
-		'Jane',
-		'New Delhi'
-	),
+			(
+			102,
+			'Smith',
+			'Jane',
+			'New Delhi'
+			),
 
-	(
-		103,
-		'Ferguson',
-		'Samantha',
-		'Kolkata'
-	),
+			(
+			103,
+			'Ferguson',
+			'Samantha',
+			'Kolkata'
+			),
 
-	(
-		104,
-		'Reynolds',
-		'Alan',
-		'Bangalore'
-	),
+			(
+			104,
+			'Reynolds',
+			'Alan',
+			'Bangalore'
+			),
 
-	(
-		105,
-		'Anderson',
-		'Page',
-		NULL
-	),
+			(
+			105,
+			'Anderson',
+			'Page',
+			NULL
+			),
 
-	(
-		106,
-		'Johnson',
-		'Derek',
-		'Chennai'
-	)
-);
+			(
+			106,
+			'Johnson',
+			'Derek',
+			'Chennai'
+	
+			);
